6829e1a78bf2495509f19734921e810011b589c7
MaxPredathor <max.gilli98@gmail.com>
2024-04-29 15:16:03 +0200

    Esercizio finito

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZipCode { get; set; }
+        public Address(string name, string surname, string street, string city, string province, string zipCode)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}, {Street} in {City}({Province}), {ZipCode}";
+        }
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Address> indirizzi = new List<Address>();
+            string path = "C:\\.NET\\addresses.csv";
+            using StreamReader stream = File.OpenText(path);
+            {
+                while (stream.EndOfStream == false)
+                {
+                    string? line = stream.ReadLine();
+                    string[] parameters = line.Split(',');
+                    try
+                    {
+                        if (parameters.Length < 6)
+                        {
+                            throw new IndexOutOfRangeException();
+                        }else if (parameters.Length > 6)
+                        {
+                            throw new IndexOutOfRangeException();
+                        }
+
+                        string name = parameters[0];
+                        string surname = parameters[1];
+                        string street = parameters[2];
+                        string city = parameters[3];
+                        string province = parameters[4];
+                        string zipCode = parameters[5];
+
+                        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname))
+                        {
+                            throw new ArgumentException();
+                        }
+
+
+                        Address indirizzo = new Address(name, surname, street, city, province, zipCode);
+                        indirizzi.Add(indirizzo);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        if (parameters.Length < 6)
+                            Console.WriteLine("Sono stati inseriti meno parametri del dovuto: " + line);
+                        else if (parameters.Length > 6)
+                            Console.WriteLine("Sono stati inseriti piu' parametri del dovuto: " + line);
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine("Nome o cognome mancante: " + line);
+                    }
+                }
+            }
+            foreach (var indirizzo in indirizzi)
+            {
+                Console.WriteLine(indirizzo.ToString());
+            }
         }
     }
 }
